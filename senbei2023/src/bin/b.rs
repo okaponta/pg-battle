1b14754aed07e73920b4a639402ee9cd321c45dc
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-21 15:20:20 +0900

    add senbei2023_b

--- senbei2023/src/bin/b.rs
+++ senbei2023/src/bin/b.rs
@@ -1,8 +1,32 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut ans = 0.0;
+    let mut dp = vec![0.0; 3];
+    dp[0] = 1.0;
+    for c in s {
+        let mut next = vec![0.0; 3];
+        if c == 'A' {
+            next = vec![0.0, 1.0, 0.0];
+        } else if c == 'B' {
+            next[2] = dp[1];
+            next[0] = 1.0 - dp[1];
+            next[1] = 0.0;
+        } else if c == 'C' {
+            ans += dp[2];
+            next = vec![1.0, 0.0, 0.0];
+        } else {
+            // ?
+            next[1] += 1.0 / 3.0;
+            next[2] += dp[1] / 3.0;
+            next[0] += (1.0 - dp[1]) / 3.0;
+            next[0] += 1.0 / 3.0;
+            ans += dp[2] / 3.0;
+        }
+        dp = next;
+    }
+    println!("{}", ans);
 }
