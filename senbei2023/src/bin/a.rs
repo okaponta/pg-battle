2bcfe488743896ab82dddea9bf6650b59623466a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-21 15:20:19 +0900

    add senbei2023_a

--- senbei2023/src/bin/a.rs
+++ senbei2023/src/bin/a.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut is_plus = true;
+    for a in a {
+        if a == 0 {
+            println!("0");
+            return;
+        }
+        if a < 0 {
+            is_plus = !is_plus;
+        }
+    }
+    println!("{}", if is_plus { "+" } else { "-" });
 }
