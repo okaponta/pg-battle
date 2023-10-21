92bbe23de15876d7f36f781a9d4e896a192f3068
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-21 11:39:18 +0900

    remove warning

--- senbei2022/src/bin/d.rs
+++ senbei2022/src/bin/d.rs
@@ -3,9 +3,9 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n:usize,
-        k:usize,
-        a:[usize;n],
-        ab:[(Usize1,Usize1);n-1],
+        _k:usize,
+        _a:[usize;n],
+        _ab:[(Usize1,Usize1);n-1],
     }
     todo!()
 }
